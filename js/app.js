9f297ca6f43c6e9c9e4593985df0cd3780927f1d
RKdev <rkdevmail@gmail.com>
2015-12-23 13:49:21 -0600

    daily commit - see below for details created basic functions left notes

--- js/app.js
+++ js/app.js
@@ -31,15 +31,45 @@ var resources = [
   },
 ];
 
-console.log(resources[0].name);
-console.log(resources[1].name);
+
+listResources();
+readArray(resources[0].category.topics);
 listTopics(0);
 
 
 //function declarations
 function listTopics(resourceID){
-  var aryLength = resources[resourceID].category.topics.length;
-  for (var i = 0; i < aryLength; i++) {
+  var intArrayLength = resources[resourceID].category.topics.length;
+  for (var i = 0; i < intArrayLength; i++) {
     console.log(resources[resourceID].category.topics[i].name);
   }
 }
+
+function listResources() {
+  var aryLength = resources.length;
+  for (var i = 0; i < aryLength; i++) {
+    console.log(resources[i].id + ' ' + resources[i].name);
+  }
+}
+
+function readArray(anyArray) {
+  for (var i = 0; i < anyArray.length; i++) {
+    console.log(anyArray[i]);
+  }
+}
+
+
+//functions to make
+
+//create record
+//  adds to record id array
+//  wants name, type, category
+//  optionally wants topics
+//  sets record id to array subscript
+//  sets cycle to 0
+
+//add topic
+//  add topic to a topic array within a record
+//  wants record to add to, category, topic name
+
+//
