7ad3b30440f1c4630616aac41403f56190763a62
RKdev <rkdevmail@gmail.com>
2016-02-21 12:15:40 -0600

    jasmine runs in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,35 @@ var gulp = require('gulp');
 
 // include plug-ins
 var jshint = require('gulp-jshint');
+var jasmine = require('gulp-jasmine');
 
-gulp.task('default', ['jshint']);
+/*gulp.task('default', ['jshint']);*/
 
+gulp.task('default', ['jasmine']);
+
+
+//jasmine
+gulp.task('jasmine', function() {
+  gulp.src('spec/jasmine_examples/*.js')
+		.pipe(jasmine({config: {
+      "spec_dir": "spec",
+      "spec_files": [
+        "**/*[sS]pec.js"
+      ],
+      "helpers": [
+        "helpers/**/*.js"
+      ],
+      "stopSpecOnExpectationFailure": false,
+      "random": false
+    }}));
+});
+
+// watch for JS changes
+gulp.watch('spec/**/*.js', function() {
+  gulp.run('jasmine');
+});
+
+/*
 // JS hint task
 gulp.task('jshint', function() {
   gulp.src(['./js/*.js', '!./js/jquery*'])
@@ -17,3 +43,5 @@ gulp.task('jshint', function() {
 gulp.watch('./js/*.js', function() {
   gulp.run('jshint');
 });
+
+*/
