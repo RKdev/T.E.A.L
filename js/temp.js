d03ea7fbb743baa19def9300610fcc9be6751f7a
RKdev <rkdevmail@gmail.com>
2016-03-21 19:14:03 -0500

    simplified topics.js logic

--- js/temp.js
+++ js/temp.js
@@ -2,10 +2,15 @@
 TEAL.topics = TEAL.topics || {};
 TEAL.topics.array = TEAL.topics.array || [];
 TEAL.topics.db = "/pretendDB/topics.json";
+TEAL.topics.categoriesName = '';
+
 
 TEAL.topics.loadtopics = function(){
-  TEAL.emptyArray(TEAL.topics.array);
-  TEAL.requestAJAX({srvReq:"rd_db", db:TEAL.topics.db, requestType:"GET", async:true, callback:TEAL.topics.populatetopicsData});
+     if (TEAL.topics.categoriesName) {
+      TEAL.requestAJAX({srvReq:"rd_db", db:TEAL.topics.db, requestType:"GET", async:true, callback:TEAL.topics.populatetopicsData});
+      } else {
+        console.log("TEAL.topics.loadtopics: no category selected");
+        }
 };
 
 TEAL.topics.writetopics = function(){
@@ -13,37 +18,47 @@ TEAL.topics.writetopics = function(){
 };
 
 TEAL.topics.populatetopicsData = function(AJAXdata){
-  var JSONdata = JSON.parse(AJAXdata);
-  TEAL.populateArray(JSONdata, TEAL.topics.array);
-  TEAL.topics.displaytopics();
+     var JSONdata = JSON.parse(AJAXdata);
+     TEAL.emptyArray(TEAL.topics.array);
+     TEAL.populateArray(JSONdata, TEAL.topics.array);
+     console.log("Hello");
+     TEAL.topics.displaytopics();
 };
 
 //create a button for each category
 TEAL.topics.displaytopics = function (){
   var aryLength = TEAL.topics.array.length;
   var topicsNameArray = [];
+  var topicsDisplayArray = [];
   var topicsHeaderDiv = document.getElementById('topics-header-area');
   var topicsButtonDiv = document.getElementById('topics-output-area');
   topicsHeaderDiv.innerHTML = TEAL.topics.categoriesName;
   topicsButtonDiv.innerHTML = '';
 
-  for (var i = 0; i < aryLength; i++) {
+  for (i = 0; i < aryLength; i++) {
     if (TEAL.topics.array[i].categoriesName === TEAL.topics.categoriesName) {
-      TEAL.addItemToArray(topicsNameArray, TEAL.topics.array[i].topicsName);
-
-      topicsButtonDiv.innerHTML = topicsButtonDiv.innerHTML +
-      '<input type="button" class="topicsButton" id=' + i + ' value=' + TEAL.topics.array[i].topicsName + '>';
+        TEAL.addItemToArray(topicsDisplayArray, TEAL.topics.array[i]);
+        console.log(topicsDisplayArray.length);
+        console.log(topicsDisplayArray);
+        console.log(topicsDisplayArray[i]);
+      //  topicsNameArray.push(topicsDisplayArray[i].topicsName);
+//        topicsButtonDiv.innerHTML = topicsButtonDiv.innerHTML +
+//        '<input type="button" class="topicsButton" id=' + i + ' value=' + topicsDisplayArray[i].topicsName + '>';
     }
   }
-  TEAL.buildDropdownFromArray('topics-drop-down', topicsNameArray);
+      TEAL.buildDropdownFromArray('topics-drop-down', topicsNameArray);
+
 };
 
 TEAL.topics.addTotopics = function (topicsName){
   document.getElementById('input-topics').value='';   //clear input box
-  TEAL.addItemToArray(TEAL.topics.array, {categoriesName: TEAL.topics.categoriesName, topicsName: topicsName});   //add item to array, redislplay UI items, store data
-  TEAL.topics.displaytopics();
-  TEAL.topics.writetopics();
-
+  if (TEAL.topics.categoriesName) {
+    TEAL.addItemToArray(TEAL.topics.array, {categoriesName: TEAL.topics.categoriesName, topicsName: topicsName});   //add item to array, redislplay UI items, store data
+//    TEAL.topics.displaytopics();
+    TEAL.topics.writetopics();
+  } else {
+    console.log("TEAL.topics.addTotopics: No Category Selected" );
+    }
 };
 
 TEAL.topics.removeFromtopics = function(){
@@ -57,5 +72,6 @@ TEAL.topics.removeFromtopics = function(){
 
 TEAL.topics.searchFromCategories = function(categoriesName){
   TEAL.topics.categoriesName = categoriesName;
+  console.log("TEAL.categories.searchFromcategories: " + TEAL.topics.categoriesName);
   TEAL.topics.loadtopics();
 };
