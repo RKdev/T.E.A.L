33e39fa85f2caa082c7f9311a4df506edf828c00
RKdev <rkdevmail@gmail.com>
2016-04-17 11:29:07 -0500

    completed view.renderPanel

--- dev/js/template.js
+++ dev/js/template.js
@@ -2,25 +2,26 @@
 (function(window){
 
 function Templates() {
-    this.buttonTemplate = '<input type="button" value="{{name}}">';
+    this.list = {
+        button : '<input type="button" value="{{name}}">',
 
-    this.dataPanelTemplate
-     = '<header><h2>{{Title}}</h2><hr/></header>'
-     + '<div id=data_panel_output></div>';
+        dataPanel :
+             '<header><h2>{{Title}}</h2><hr/></header>'
+            + '<div id=data_panel_output></div>',
 
-     this.controlPanelTemplate
-     = '<header><h2>{{Title}}</h2><hr/></header>'
-     + '<div id="control_panel_features">'
-     + '<input id="input_box" type="text">'
-     + '<input type="button" value="Add">'
-     + '<input type="button" value="Delete">'
-     + '</div>';
+        controlPanel :
+             '<header><h2>{{Title}}</h2><hr/></header>'
+            + '<div id="control_panel_features">'
+            + '<input id="input_box" type="text">'
+            + '<input type="button" value="Add">'
+            + '<input type="button" value="Delete">'
+            + '</div>'
+    };
 }
 
 Templates.prototype.display = function(template, data){
     var view = '';
-    var returnTemplate = '';
-
+    var returnTemplate = this.list[template];
 
     view = view + returnTemplate;
     return view;
--- dev/js/view.js
+++ dev/js/view.js
@@ -1,17 +1,17 @@
 (function(window){
 
-    function View(templatesJS){
-        this.templates = templatesJS;
+    function View(templates){
+        this.templates = templates;
 
         //get a list of panels from somewhere
-        this.$panel1 = geid('panel_1');
-        this.$panel2 = geid('panel_2');
+        this.panels = {
+                panel1 : geid('panel_1'),
+                panel2 : geid('panel_2')
+        };
     }
 
 View.prototype.renderPanel = function(panelRef, templateRef){
-        this.$panel1.innerHTML = this.templates.dataPanelTemplate;
-        this.$panel2.innerHTML = this.templates.controlPanelTemplate;
-
+        this.panels[panelRef].innerHTML = this.templates.display(templateRef);
 };
 
 window.TEAL = window.TEAL || {};
