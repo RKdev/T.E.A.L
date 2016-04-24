005c6365bce6cba1d13b8c01207216fd9b455168
RKdev <rkdevmail@gmail.com>
2016-04-24 15:37:59 -0500

    view.js populates dynamically from index.html

--- dev/js/view.js
+++ dev/js/view.js
@@ -3,17 +3,20 @@
     function View(templates){
         this.templates = templates;
 
-        //get a list of panels from somewhere
-        //this will probably turn into an array
+        //get a list of panels getLayout method
         this.panels = {};
 }
 
 View.prototype.getLayout = function() {
         //this will eventually take a "layoutconfig.json" argument and provide the layout
         // for the html or present a default option if no config is passed
-        this.panels.panel_1 = 'panel_1';
-        this.panels.panel_2 = 'panel_2';
-        this.panels.data_panel_output = 'data_panel_output';
+        // for now retrieves div list from index.html
+        var divs = qsa('div');
+
+        for (var i = 0; i < divs.length; i++){
+              var panelName = 'panel_' + (i + 1).toString();
+              this.panels[panelName] = divs[i].id;
+        }
 };
 
 View.prototype.renderPanel = function(panelRef, templateRef){
