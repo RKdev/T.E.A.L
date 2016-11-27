0ef3282fbcfef644e24851089e2d50ca7d428c36
Keys <keys@austin.utexas.edu>
2016-11-27 13:38:51 -0600

    Generalized load function based on UIContext

--- js/model.js
+++ js/model.js
@@ -10,10 +10,6 @@ Model.prototype.setUIContext = function (context) {
     this.UIContext = context;
 };
 
-String.prototype.capitalize = function(){
-       return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
-      };
-
 Model.prototype.readGeneral = function (callback){
   this.store.readFile(this.store[this.UIContext], callback);
   return ('teal.model.readGeneral');
