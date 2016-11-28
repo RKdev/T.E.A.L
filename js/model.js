8b97dcead403eec73faf5fcc89bf7f07b4873386
Keys <keys@austin.utexas.edu>
2016-11-28 13:56:59 -0600

    removed unused functions from model.js

--- js/model.js
+++ js/model.js
@@ -20,16 +20,6 @@ Model.prototype.addData = function (data, callback) {
       return('teal.model.addData');
 };
 
-Model.prototype.listTopicsByCategory = function (category, callback) {
-    //generate a list of all topics within a category
-    //pass that list to a callback function
-};
-
-Model.prototype.listResourcesByTopic = function (topic, callback) {
-    //generate a list of all resources withing a topic
-    //pass that list to a callback function
-};
-
 window.TEALClass = window.TEALClass || {};
 window.TEALClass.model = Model;
 })(window);
